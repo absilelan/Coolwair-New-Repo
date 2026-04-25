416df6d74818a9edf0b7c856f4566f0047230970
absilelan <32073041+absilelan@users.noreply.github.com>
2026-04-26 01:32:49 +0530

    Instagram link updated

--- components.js
+++ components.js
@@ -53,7 +53,7 @@ const CW = {
           </svg>
         </a>
 
-        <a href="https://www.instagram.com/coollwair/" target="_blank" class="social-link" aria-label="Instagram">
+        <a href="https://www.instagram.com/coolwair/" target="_blank" class="social-link" aria-label="Instagram">
           <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" xmlns="http://www.w3.org/2000/svg">
             <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
             <circle cx="12" cy="12" r="4"></circle>
